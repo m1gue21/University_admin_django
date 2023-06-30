09b7e1410ad2d9293b4ab2f7569f5d0b4ae4e4a7
m1gue21 <miguefreeagent@gmail.com>
2023-06-30 18:39:03 -0500

    models updated
    display names added  to the models

--- Modules/Academic/models.py
+++ Modules/Academic/models.py
@@ -8,6 +8,10 @@ class Career(models.Model):
     name = models.CharField(max_length=50)
     duration = models.PositiveSmallIntegerField(default=5)
 
+    def __str__(self):
+        career_display_name = "{0}(Duration: {1} years)"
+        return career_display_name.format(self.name, self.duration)
+
 
 class Student(models.Model):
     dni = models.CharField(max_length=8, primary_key=True)
@@ -24,8 +28,16 @@ class Student(models.Model):
     vigency = models.BooleanField(default=True)
 
     def get_full_name(self):
-        full_name = {"{0} {1}, {2}"}
-        return full_name.format(self.maternal_surname, self.paternal_surname, self.names)
+        full_name = "{0} {1}, {2}"
+        return full_name.format(self.paternal_surname, self.maternal_surname, self.names)
+
+    def __str__(self):
+        display_text = "{0} / Career: {1} / {2}"
+        if self.vigency:
+            student_state = "Active"
+        else:
+            student_state = "Inactive"
+        return display_text.format(self.get_full_name(), self.career, student_state)
 
 
 class Course(models.Model):
@@ -34,9 +46,18 @@ class Course(models.Model):
     credits = models.PositiveSmallIntegerField()
     teacher = models.CharField(max_length=100)
 
+    def __str__(self):
+        display_text = "{0} ({1}) / Teacher: {2}"
+        return display_text.format(self.name, self.id, self.teacher)
+
 
 class Enrollment(models.Model):
     id = models.AutoField(primary_key=True)
     student = models.ForeignKey(Student, null=False, blank=False, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, null=False, blank=False, on_delete=models.CASCADE)
     enrollment_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        display_text = "{0} enrolled to the course {1} / Date: {2}"
+        enrollment_date = self.enrollment_date.strftime("%A %d/%m/%Y %H:%M:%S")
+        return display_text.format(self.student.get_full_name(), self.course, enrollment_date)
